73556c1e9b76f2ab799f4c2a5b019ad46fc4000d
DaffaAfifi <afifidaffa16@gmail.com>
2022-11-29 00:52:41 +0700

    feat: create db tables & models for custom

---
+++ database/migrations/2022_11_28_173553_create_custom_details_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('custom_details', function (Blueprint $table) {
+            $table->unsignedBigInteger('transaction_id');
+            $table->foreign('transaction_id')->references('id')->on('transactions')->onDelete('restrict');
+            $table->unsignedBigInteger('custom_id');
+            $table->foreign('custom_id')->references('id')->on('customs')->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('custom_details');
+    }
+};
