47d17a18f9e1e6cbe0ffe9a8898ffbf3624781e5
Raihan <17radf@gmail.com>
2022-12-29 12:08:24 +0700

    feat(api): stok furnitur

--- app/Http/Controllers/ApiDetailOrderController.php
+++ app/Http/Controllers/ApiDetailOrderController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use App\Models\Transaction;
 use App\Models\TransactionDetail;
 use Illuminate\Http\Request;
@@ -10,6 +11,7 @@ class ApiDetailOrderController extends Controller
 {
   public function index()
   {
+
     $id = auth()->user()->id;
 
     $ongoing = Transaction::find(auth()->user()->transactions()
@@ -30,7 +32,17 @@ class ApiDetailOrderController extends Controller
       ->where('status', "Pending")
       ->where('categories', "Product")
       ->latest('id')->first()->id);
-    return response()->json(['results' => $ongoing->products()->attach($request->product_id)]);
+
+    $product = Product::find($request->product_id);
+
+    if ($product->qty > 0) {
+      $product->qty -= 1;
+      $product->update();
+
+      return response()->json(['results' => $ongoing->products()->attach($request->product_id)]);
+    } else {
+      return response()->json(['message' => "Furnitur kosong"]);
+    }
   }
 
   public function show($id)
@@ -40,6 +52,7 @@ class ApiDetailOrderController extends Controller
 
   public function update(Request $request)
   {
+
     $data = [
       'qty' => $request->qty,
       'sub_total' => $request->sub_total
@@ -50,6 +63,24 @@ class ApiDetailOrderController extends Controller
       ->where('categories', "Product")
       ->latest('id')->first()->id);
 
+    $product = Product::find($request->product_id);
+    $detail = TransactionDetail::where('transaction_id', auth()->user()->transactions()
+      ->where('status', "Pending")
+      ->where('categories', "Product")
+      ->latest('id')->first()->id)
+      ->where('product_id', $request->product_id)->first();
+
+    // dd("requestnya {$request->qty}, di isinya {$detail->qty}");
+    if ($request->qty > $detail->qty) {
+      if ($product->qty > 0) {
+        $product->qty -= 1;
+      } else {
+        return response()->json(['message' => "Furnitur kosong"], 403);
+      }
+    } else {
+      $product->qty += 1;
+    }
+    $product->update();
     return response()->json(['results' => $ongoing->products()->updateExistingPivot($request->product_id, $data)]);
   }
 
